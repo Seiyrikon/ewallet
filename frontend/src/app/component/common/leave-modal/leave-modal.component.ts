d848d651e690e34ccec3904ed31a317f3f6bb1c3
Seiyrikon <rickygalpo7777@gmail.com>
2023-12-31 13:51:51 +0800

    created modal component for leave action

---
+++ frontend/src/app/component/common/leave-modal/leave-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-modal',
+  templateUrl: './leave-modal.component.html',
+  styleUrls: ['./leave-modal.component.css']
+})
+export class LeaveModalComponent {
+
+}
