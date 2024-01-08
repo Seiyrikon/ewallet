0435ec7fca781252304378625a86e8a67f1aa8bc
Seiyrikon <rickygalpo7777@gmail.com>
2024-01-08 17:11:48 +0800

    added tbl_user_friends_mst and tbl_friend_request_mst

--- database/EWALLET_DB.sql
+++ database/EWALLET_DB.sql
@@ -9,7 +9,7 @@ CREATE TABLE tbl_user_mst (
     username VARCHAR(20) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL,
-    del_flag int(1) NOT NULL DEFAULT 0,
+    del_flag INT(1) NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL
 );
@@ -21,7 +21,7 @@ CREATE TABLE tbl_personal_info_mst(
     first_name VARCHAR(20) NOT NULL,
     middle_name VARCHAR(20) NULL,
     last_name VARCHAR(20) NULL,
-    del_flag int(1) NULL DEFAULT 0,
+    del_flag INT(1) NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL,
     FOREIGN KEY (user_id) REFERENCES tbl_user_mst (user_id)
@@ -33,7 +33,7 @@ CREATE TABLE tbl_wallet_mst(
     user_id INT(9) UNSIGNED NOT NULL,
     wallet_name VARCHAR(50) NOT NULL,
     wallet_desc VARCHAR(255),
-    del_flag int(1) NOT NULL DEFAULT 0,
+    del_flag INT(1) NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL,
     FOREIGN KEY(user_id) REFERENCES tbl_user_mst (user_id)
@@ -46,7 +46,7 @@ CREATE TABLE tbl_withdraw(
     wallet_id INT(9) UNSIGNED NOT NULL,
     amount DOUBLE NOT NULL,
     withdraw_desc VARCHAR(255),
-    del_flag int(1) NOT NULL DEFAULT 0,
+    del_flag INT(1) NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL,
     FOREIGN KEY(user_id) REFERENCES tbl_user_mst (user_id),
@@ -60,7 +60,7 @@ CREATE TABLE tbl_deposit(
     wallet_id INT(9) UNSIGNED NOT NULL,
     amount DOUBLE NOT NULL,
     deposit_desc VARCHAR(255),
-    del_flag int(1) NOT NULL DEFAULT 0,
+    del_flag INT(1) NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL,
     FOREIGN KEY(user_id) REFERENCES tbl_user_mst (user_id),
@@ -75,9 +75,34 @@ CREATE TABLE tbl_transaction(
     transaction_type VARCHAR(10) NOT NULL,
     transaction_amount DOUBLE NOT NULL,
     transaction_desc VARCHAR(255),
-    del_flag int(1) NOT NULL DEFAULT 0,
+    del_flag INT(1) NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL,
     FOREIGN KEY(user_id) REFERENCES tbl_user_mst (user_id),
     FOREIGN KEY(wallet_id) REFERENCES tbl_wallet_mst(wallet_id)
+);
+
+DROP TABLE IF EXISTS tbl_user_friend_mst;
+CREATE TABLE tbl_user_friend_mst(
+	ur_id INT(9) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT(9) UNSIGNED NOT NULL,
+    friend_id INT(9) UNSIGNED NOT NULL,
+    block_flag INT(1) NOT NULL DEFAULT 0,
+    del_flag INT(1) NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL,
+    FOREIGN KEY(user_id) REFERENCES tbl_user_mst (user_id),
+    FOREIGN KEY(friend_id) REFERENCES tbl_user_mst (user_id)
+);
+
+DROP TABLE IF EXISTS tbl_friend_request_mst;
+CREATE TABLE tbl_friend_request_mst (
+	fr_id INT(9) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    requestor_id INT(9) UNSIGNED NOT NULL,
+    requestee_id INT(9) UNSIGNED NOT NULL,
+    del_flag INT(1) NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL,
+    FOREIGN KEY(requestor_id) REFERENCES tbl_user_mst (user_id),
+    FOREIGN KEY(requestee_id) REFERENCES tbl_user_mst (user_id)
 );
