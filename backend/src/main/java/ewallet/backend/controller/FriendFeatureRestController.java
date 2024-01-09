351ee0f285d3b76019b20727c43e58e830a5d42e
Seiyrikon <rickygalpo7777@gmail.com>
2024-01-09 16:06:52 +0800

    cleanup

--- backend/src/main/java/ewallet/backend/controller/FriendFeatureRestController.java
+++ backend/src/main/java/ewallet/backend/controller/FriendFeatureRestController.java
@@ -39,4 +39,5 @@ public class FriendFeatureRestController
     {
         return friendFeatureService.declineFriendRequest(friendId);
     }
+    
 }
--- backend/src/main/java/ewallet/backend/controller/tbl_friend_request_mstRestController.java
+++ backend/src/main/java/ewallet/backend/controller/tbl_friend_request_mstRestController.java
@@ -11,8 +11,6 @@ import ewallet.backend.service.tbl_friend_request_mstService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
