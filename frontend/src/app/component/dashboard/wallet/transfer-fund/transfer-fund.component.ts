a18c2a79d62db587a1fc31a1d1d7827381706da2
Seiyrikon <rickygalpo7777@gmail.com>
2024-01-26 14:56:37 +0800

    changed color pallete to black

--- frontend/src/app/component/dashboard/wallet/transfer-fund/transfer-fund.component.ts
+++ frontend/src/app/component/dashboard/wallet/transfer-fund/transfer-fund.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { DeleteModalComponent } from 'src/app/component/common/delete-modal/delete-modal.component';
 import { Principal } from 'src/app/interface/principal';
+import { Wallet } from 'src/app/interface/wallet';
 import { PrincipalService } from 'src/app/service/principal/principal.service';
+import { WalletService } from 'src/app/service/wallet/wallet.service';
 
 @Component({
   selector: 'app-transfer-fund',
@@ -12,13 +19,20 @@ export class TransferFundComponent implements OnInit, OnDestroy
 {
 
   private _subscription!: Subscription;
+  wallets!: Wallet[];
   principal!: Principal;
+  ownWalletForm!: FormGroup;
   errorMessage: string = '';
   showProgressBar: boolean = false;
+  isOwnWalletFormVisible: boolean = false;
 
   constructor
   (
     private _principalService: PrincipalService,
+    private _walletService: WalletService,
+    private _router: Router,
+    private _dialog: MatDialog,
+    private _snackbar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -52,9 +66,53 @@ export class TransferFundComponent implements OnInit, OnDestroy
     )
   }
 
-  onOwn(userId: any):any {
-    console.log(userId);
+  getAllUserWallet(): any
+  {
+    this.showProgressBar = true;
 
+    const wallets$ = this._walletService.getAllUserWallet()
+
+    wallets$.subscribe
+    (
+      (response) => {
+        if (!response)
+        {
+          console.error('Response is empty');
+        }
+        this.wallets = response.message;
+      },
+      (error) => {
+        console.error('Get Wallet Failed', error);
+        this.errorMessage = error;
+        this.showProgressBar = false;
+      },
+      () => {
+        this.showProgressBar = false;
+      }
+    )
+  }
+
+  onOwnClick():any {
+    this.isOwnWalletFormVisible = !this.isOwnWalletFormVisible;
+    this.getAllUserWallet();
+  }
+
+  onFriendWalletClick():any {
+    this.isOwnWalletFormVisible = false;
+  }
+
+  onOtherClick():any {
+    this.isOwnWalletFormVisible = false;
+  }
+
+  onOwnWalletSubmit(): any
+  {
+
+  }
+
+  onView(walletId: number): any {
+    console.log(walletId);
+    this._router.navigate(['/dashboard', { outlets: { contentOutlet: ['wallet', 'view', `${walletId}`] } }]);
   }
 
   ngOnDestroy(): void {
