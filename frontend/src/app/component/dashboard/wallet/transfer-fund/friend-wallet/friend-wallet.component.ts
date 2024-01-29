8c14d17b035b1c7ae98975e2d74600eaa6ba5a37
Seiyrikon <rickygalpo7777@gmail.com>
2024-01-29 15:31:07 +0800

    implementing transfer fund for friend's wallet

--- frontend/src/app/component/dashboard/wallet/transfer-fund/friend-wallet/friend-wallet.component.ts
+++ frontend/src/app/component/dashboard/wallet/transfer-fund/friend-wallet/friend-wallet.component.ts
@@ -1,10 +1,135 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Friend } from 'src/app/interface/friend';
+import { Wallet } from 'src/app/interface/wallet';
+import { FriendService } from 'src/app/service/friend/friend.service';
+import { WalletService } from 'src/app/service/wallet/wallet.service';
 
 @Component({
   selector: 'app-friend-wallet',
   templateUrl: './friend-wallet.component.html',
   styleUrls: ['./friend-wallet.component.css']
 })
-export class FriendWalletComponent {
+export class FriendWalletComponent implements OnInit, OnDestroy
+{
 
+  private _subscription!: Subscription
+  searchedFriends!: Friend[] | null;
+  friends!: Friend[];
+  wallets!: Wallet[];
+  searchText!: string;
+  errorMessage: string = '';
+  showProgressBar: boolean = false;
+  showSubmitButton: boolean = true;
+  transferFromId: number = 0;
+  transferToId: number = 0;
+  isSubmitted: boolean = false;
+
+  constructor
+  (
+    private _friendService: FriendService,
+    private _walletService: WalletService,
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllFriendsOfUser();
+    this.getAllUserWallet();
+    // console.log(this.friendRequestChecker(2));
+
+  }
+
+  getAllFriendsOfUser(): any {
+      this.showProgressBar = true;
+      const friends$ = this._friendService.getAllFriendsOfUser();
+
+      friends$.subscribe
+      (
+        (response) => {
+          if(!response)
+          {
+            console.error('Response is empty');
+          }
+          this.friends = response.message;
+          this.errorMessage = ''
+        },
+        (error) => {
+          console.error('An error occured', error);
+          this.errorMessage = error;
+          this.showProgressBar = false;
+        },
+        () => {
+          this.showProgressBar = false;
+          this.errorMessage = ''
+          console.log(this.friends);
+
+        }
+      )
+  }
+
+  getAllUserWallet(): any
+  {
+    this.showProgressBar = true;
+
+    const wallets$ = this._walletService.getAllUserWallet()
+
+    wallets$.subscribe
+    (
+      (response) => {
+        if (!response)
+        {
+          console.error('Response is empty');
+        }
+        this.wallets = response.message;
+      },
+      (error) => {
+        console.error('Get Wallet Failed', error);
+        this.errorMessage = error;
+        this.showProgressBar = false;
+      },
+      () => {
+        this.showProgressBar = false;
+      }
+    )
+  }
+
+  getSanitizedImage(image: any): any {
+    // Assuming profilePicture is a base64 string
+    const imageSrc = `data:image/png;base64,${image}`;
+    return this._sanitizer.bypassSecurityTrustResourceUrl(imageSrc);
+  }
+
+  onFromSelect(walletId: number)
+  {
+    this.transferFromId = 0;
+    this.transferFromId = walletId;
+    console.log(walletId);
+
+  }
+  onToSelect(walletId: number)
+  {
+    this.transferToId = 0;
+    this.transferToId = walletId;
+    console.log(walletId);
+  }
+
+  onDeselectFrom()
+  {
+    this.transferFromId = 0;
+  }
+  onDeselectTo()
+  {
+    this.transferToId = 0;
+  }
+
+  ngOnDestroy(): void {
+    if(this._subscription)
+    {
+      this._subscription.unsubscribe();
+    }
+  }
 }
