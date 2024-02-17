4e06d9e5e0adf8dc01f666c5cecd61cd7c9efa35
Seiyrikon <rickygalpo7777@gmail.com>
2024-02-17 14:34:05 +0800

    proper handling of expired session

--- frontend/src/app/component/dashboard/wallet/transfer-fund/own-wallet/own-wallet.component.ts
+++ frontend/src/app/component/dashboard/wallet/transfer-fund/own-wallet/own-wallet.component.ts
@@ -10,6 +10,7 @@ import { Wallet } from 'src/app/interface/wallet';
 import { PrincipalService } from 'src/app/service/principal/principal.service';
 import { WalletService } from 'src/app/service/wallet/wallet.service';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { ExpiredSessionComponent } from 'src/app/component/common/expired-session/expired-session.component';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -73,6 +74,8 @@ export class OwnWalletComponent {
         console.error('Principal Info not found', error);
         this.errorMessage = error;
         this.showProgressBar = false;
+        this.openExpiredSessionDialog();
+        this._router.navigate(['/login']);
       },
       () => {
         this.showProgressBar = false;
@@ -117,6 +120,8 @@ export class OwnWalletComponent {
         console.error('Get Wallet Failed', error);
         this.errorMessage = error;
         this.showProgressBar = false;
+        this.openExpiredSessionDialog();
+        this._router.navigate(['/login']);
       },
       () => {
         this.showProgressBar = false;
@@ -191,6 +196,21 @@ export class OwnWalletComponent {
         }
       );
     }
+  }
+
+  openExpiredSessionDialog(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      const dialogRef = this._dialog.open(ExpiredSessionComponent);
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result === true) {
+          resolve(true); // User confirmed leaving
+          this._router.navigate(['/login']);
+        } else {
+          resolve(false); // User canceled leaving
+        }
+      });
+    });
   }
 
   ngOnDestroy(): void {
