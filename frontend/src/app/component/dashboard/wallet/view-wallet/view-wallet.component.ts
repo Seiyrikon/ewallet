b79dcf19d6df79cf55d0131c318cab98fc792f08
Seiyrikon <rickygalpo7777@gmail.com>
2024-02-17 14:16:40 +0800

    proper handling of expired session

--- frontend/src/app/component/dashboard/wallet/view-wallet/view-wallet.component.ts
+++ frontend/src/app/component/dashboard/wallet/view-wallet/view-wallet.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { ExpiredSessionComponent } from 'src/app/component/common/expired-session/expired-session.component';
 import { Wallet } from 'src/app/interface/wallet';
 import { WalletService } from 'src/app/service/wallet/wallet.service';
 
@@ -21,7 +23,8 @@ export class ViewWalletComponent implements OnInit, OnDestroy
   (
     private _walletService: WalletService,
     private _route: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private _dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -48,6 +51,8 @@ export class ViewWalletComponent implements OnInit, OnDestroy
           console.error('An error occured', error);
           this.errorMessage = error;
           this.showProgressBar = false;
+          this.openExpiredSessionDialog();
+          this._router.navigate(['/login']);
         },
         () => {
           this.showProgressBar = false;
@@ -73,6 +78,21 @@ export class ViewWalletComponent implements OnInit, OnDestroy
   onBack()
   {
     this._router.navigate(['/dashboard', { outlets: { contentOutlet: ['wallet'] } }]);
+  }
+
+  openExpiredSessionDialog(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      const dialogRef = this._dialog.open(ExpiredSessionComponent);
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result === true) {
+          resolve(true); // User confirmed leaving
+          this._router.navigate(['/login']);
+        } else {
+          resolve(false); // User canceled leaving
+        }
+      });
+    });
   }
 
   ngOnDestroy(): void {
