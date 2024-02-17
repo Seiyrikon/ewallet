5f98fec2ea9418bac6632b96f5e4aeee315b5e25
Seiyrikon <rickygalpo7777@gmail.com>
2024-02-17 15:03:41 +0800

    proper handling of expired session

--- frontend/src/app/component/dashboard/search/search/search.component.ts
+++ frontend/src/app/component/dashboard/search/search/search.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { ExpiredSessionComponent } from 'src/app/component/common/expired-session/expired-session.component';
 import { Friend } from 'src/app/interface/friend';
 import { User } from 'src/app/interface/user';
 import { FriendService } from 'src/app/service/friend/friend.service';
@@ -28,7 +30,8 @@ export class SearchComponent implements OnInit, OnDestroy
     private _userService: UserService,
     private _route: ActivatedRoute,
     private _router: Router,
-    private _sanitizer: DomSanitizer
+    private _sanitizer: DomSanitizer,
+    private _dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -54,6 +57,8 @@ export class SearchComponent implements OnInit, OnDestroy
         console.error('An error occured', error);
         this.errorMessage = error;
         this.showProgressBar = false;
+        this.openExpiredSessionDialog();
+        this._router.navigate(['/login']);
       },
       () => {
         this.showProgressBar = false;
@@ -188,6 +193,21 @@ export class SearchComponent implements OnInit, OnDestroy
     // Assuming profilePicture is a base64 string
     const imageSrc = `data:image/png;base64,${image}`;
     return this._sanitizer.bypassSecurityTrustResourceUrl(imageSrc);
+  }
+
+  openExpiredSessionDialog(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      const dialogRef = this._dialog.open(ExpiredSessionComponent);
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result === true) {
+          resolve(true); // User confirmed leaving
+          this._router.navigate(['/login']);
+        } else {
+          resolve(false); // User canceled leaving
+        }
+      });
+    });
   }
 
   ngOnDestroy(): void {
