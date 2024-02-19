8ce4d53b3c21d24214cfb0d97be76d60a105abac
Seiyrikon <rickygalpo7777@gmail.com>
2024-02-19 08:57:58 +0800

    proper handling of expired session

--- frontend/src/app/component/dashboard/wallet/transfer-fund/transfer-fund.component.ts
+++ frontend/src/app/component/dashboard/wallet/transfer-fund/transfer-fund.component.ts
@@ -6,9 +6,11 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { DeleteModalComponent } from 'src/app/component/common/delete-modal/delete-modal.component';
+import { ExpiredSessionComponent } from 'src/app/component/common/expired-session/expired-session.component';
 import { Principal } from 'src/app/interface/principal';
 import { Transfer } from 'src/app/interface/transfer';
 import { Wallet } from 'src/app/interface/wallet';
+import { AuthService } from 'src/app/service/auth/auth.service';
 import { PrincipalService } from 'src/app/service/principal/principal.service';
 import { WalletService } from 'src/app/service/wallet/wallet.service';
 
@@ -38,6 +40,7 @@ export class TransferFundComponent implements OnInit, OnDestroy
   transferFromId: number = 0;
   transferToId: number = 0;
   isSubmitted: boolean = false;
+  session!: any;
 
   amount!: FormControl;
   note!: FormControl;
@@ -50,10 +53,37 @@ export class TransferFundComponent implements OnInit, OnDestroy
     private _walletService: WalletService,
     private _router: Router,
     private _dialog: MatDialog,
-    private _snackbar: MatSnackBar
+    private _snackbar: MatSnackBar,
+    private _authService: AuthService
   ) {}
 
   ngOnInit(): void {
+    this.isSessionExpired();
+  }
+
+  isSessionExpired(): any {
+
+    const session$ = this._authService.checkSession();
+
+    session$.subscribe
+    (
+      (response) => {
+        if(!response)
+        {
+          console.error('Response is empty');
+        }
+        this.session = response.message;
+      },
+      (error) => {
+        console.error('Sesssion is expired', error);
+        this.openExpiredSessionDialog();
+        this._router.navigate(['/login']);
+      },
+      () => {
+        console.log("Session: ", this.session);
+
+      }
+    )
   }
 
   onOwnClick():any {
@@ -70,6 +100,21 @@ export class TransferFundComponent implements OnInit, OnDestroy
   onOtherClick():any {
     this._router.navigate(['/dashboard', { outlets: { contentOutlet: ['wallet', 'transfer', {outlets: { transferFundOutlet: ['other']}}] } }]);
     this.isOwnWalletClicked = false;
+  }
+
+  openExpiredSessionDialog(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      const dialogRef = this._dialog.open(ExpiredSessionComponent);
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result === true) {
+          resolve(true); // User confirmed leaving
+          this._router.navigate(['/login']);
+        } else {
+          resolve(false); // User canceled leaving
+        }
+      });
+    });
   }
 
   ngOnDestroy(): void {
