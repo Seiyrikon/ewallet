6868a03d92e5a16c601c6c53463f3489d3b97e3e
Seiyrikon <rickygalpo7777@gmail.com>
2024-02-19 08:46:49 +0800

    proper handling of expired session

--- frontend/src/app/component/dashboard/chat-session/chat-session/chat-session.component.ts
+++ frontend/src/app/component/dashboard/chat-session/chat-session/chat-session.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { ExpiredSessionComponent } from 'src/app/component/common/expired-session/expired-session.component';
 import { ChatSession } from 'src/app/interface/chat-session';
+import { AuthService } from 'src/app/service/auth/auth.service';
 import { ChatService } from 'src/app/service/chat/chat.service';
 
 @Component({
@@ -17,18 +20,47 @@ export class ChatSessionComponent implements OnInit, OnDestroy {
   showProgressBar: boolean = false;
   recipient_id!: any;
   chatText!: string;
+  session!: any;
 
   constructor
     (
       private _chatService: ChatService,
       private _route: ActivatedRoute,
       private _router: Router,
-      private _sanitizer: DomSanitizer
+      private _sanitizer: DomSanitizer,
+      private _dialog: MatDialog,
+      private _authService: AuthService
     ) { }
 
   ngOnInit(): void {
+    this.isSessionExpired();
     this.recipient_id = this._route.snapshot.paramMap.get('recipient_id');
     this.getChatSession();
+  }
+
+  isSessionExpired(): any {
+
+    const session$ = this._authService.checkSession();
+
+    session$.subscribe
+    (
+      (response) => {
+        if(!response)
+        {
+          console.error('Response is empty');
+        }
+        this.session = response.message;
+      },
+      (error) => {
+        console.error('Sesssion is expired', error);
+        this.openExpiredSessionDialog();
+        this._router.navigate(['/login']);
+      },
+      () => {
+        console.log("Session: ", this.session);
+
+      }
+    )
   }
 
   getChatSession(): any {
@@ -95,6 +127,21 @@ export class ChatSessionComponent implements OnInit, OnDestroy {
     // Assuming profilePicture is a base64 string
     const imageSrc = `data:image/png;base64,${image}`;
     return this._sanitizer.bypassSecurityTrustResourceUrl(imageSrc);
+  }
+
+  openExpiredSessionDialog(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      const dialogRef = this._dialog.open(ExpiredSessionComponent);
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result === true) {
+          resolve(true); // User confirmed leaving
+          this._router.navigate(['/login']);
+        } else {
+          resolve(false); // User canceled leaving
+        }
+      });
+    });
   }
 
   ngOnDestroy(): void {
