0dd77f1e4a852fc3ef17402c081d672a3b59a29e
Seiyrikon <rickygalpo7777@gmail.com>
2024-02-19 08:57:47 +0800

    proper handling of expired session

--- frontend/src/app/component/dashboard/transaction/transaction.component.ts
+++ frontend/src/app/component/dashboard/transaction/transaction.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 import { Transaction } from 'src/app/interface/transaction';
+import { AuthService } from 'src/app/service/auth/auth.service';
 import { TransactionService } from 'src/app/service/transaction/transaction.service';
+import { ExpiredSessionComponent } from '../../common/expired-session/expired-session.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-transaction',
@@ -14,14 +18,44 @@ export class TransactionComponent implements OnInit, OnDestroy
   transactions!: Transaction[];
   errorMessage: string = '';
   showProgressBar: boolean = false;
+  session!: any;
 
   constructor
   (
     private _transactionService: TransactionService,
+    private _dialog: MatDialog,
+    private _authService: AuthService,
+    private _router: Router
   ) {}
 
   ngOnInit(): void {
+    this.isSessionExpired();
     this.getAllTransactionPerUser();
+  }
+
+  isSessionExpired(): any {
+
+    const session$ = this._authService.checkSession();
+
+    session$.subscribe
+    (
+      (response) => {
+        if(!response)
+        {
+          console.error('Response is empty');
+        }
+        this.session = response.message;
+      },
+      (error) => {
+        console.error('Sesssion is expired', error);
+        this.openExpiredSessionDialog();
+        this._router.navigate(['/login']);
+      },
+      () => {
+        console.log("Session: ", this.session);
+
+      }
+    )
   }
 
   getAllTransactionPerUser(): any
@@ -48,6 +82,21 @@ export class TransactionComponent implements OnInit, OnDestroy
         this.showProgressBar = false
       }
     )
+  }
+
+  openExpiredSessionDialog(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      const dialogRef = this._dialog.open(ExpiredSessionComponent);
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result === true) {
+          resolve(true); // User confirmed leaving
+          this._router.navigate(['/login']);
+        } else {
+          resolve(false); // User canceled leaving
+        }
+      });
+    });
   }
 
   ngOnDestroy(): void {
